6e0cfab6341d42fba7b666d7439e5c3b050b158c
Joshua Raichur <hello@joshuaraichur.com>
2015-07-13 07:45:46 +0530

    test undefined

--- js/tests/utilstest.js
+++ js/tests/utilstest.js
@@ -8,6 +8,7 @@ define([
     test('Test celsius to fahrenheit calculation', function () {
       strictEqual(utils.celsiusToFahrenheit(32), 89.6, 'Test a number');
       strictEqual(utils.celsiusToFahrenheit('hot'), null, 'Test a string');
+      strictEqual(utils.celsiusToFahrenheit(), null, 'Test undefined');
     });
   };
 
