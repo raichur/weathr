37a68ef0db0d0f06cf95790dcf90f53a018f83a6
Joshua Raichur <hello@joshuaraichur.com>
2015-07-13 12:33:46 +0530

    oops, places model

--- js/app/collections/places.js
+++ js/app/collections/places.js
@@ -1,14 +1,15 @@
 define([
   'backbone',
+  'app/models/place'
 ], function (Backbone, PlaceModel) {
 
   'use strict';
 
   var PlaceCollection = Backbone.Collection.extend({
-    localStorage: new Backbone.localStorage("Places"),
+    localStorage: new Backbone.LocalStorage("Places"),
     model: PlaceModel
   });
 
-  return PlacesCollection;
+  return PlaceCollection;
 
 });
