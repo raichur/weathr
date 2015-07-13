2f5b57e9e175355035ca36fdfce27dc2dd77cac7
Joshua Raichur <hello@joshuaraichur.com>
2015-07-13 12:14:06 +0530

    fetch

--- js/app/models/day.js
+++ js/app/models/day.js
@@ -11,6 +11,16 @@ define([
       'highFahrenheit': null,
       'lowFahrenheit': null,
       'icon_url': ''
+    },
+    parse: function (data) {
+      var map = {
+        'highCelsius': data.high.celsius,
+        'lowCelsius': data.low.celsius,
+        'highFahrenheit': data.high.fahrenheit,
+        'lowFahrenheit': data.low.fahrenheit,
+        'icon_url': data.icon_url,
+      };
+      return map;
     }
   });
 
