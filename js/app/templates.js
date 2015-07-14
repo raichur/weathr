79807e481e97ae56fee1d93a9e9fc6d33b03d50e
Joshua Raichur <hello@joshuaraichur.com>
2015-07-14 20:01:30 +0530

    close buttons

--- js/app/templates.js
+++ js/app/templates.js
@@ -34,7 +34,7 @@ define([
       '<div class="modal-dialog">',
         '<div class="modal-content">',
           '<div class="modal-header">',
-            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>',
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>',
             '<h4 class="modal-title"><%= title %></h4>',
           '</div>',
           '<div class="modal-body">test</div>',
